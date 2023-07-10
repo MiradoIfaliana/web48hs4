9a049e8dcf1fdf0761bade655ea90937d97d8d5b
MiradoIfaliana <119670521+MiradoIfaliana@users.noreply.github.com>
2023-07-10 16:36:49 +0000

    ok

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -30,9 +30,30 @@ class First extends CI_Controller {
 		$password=$this->input->post('password');
 		$user=$this->Login->getUser($mail,$password);
 		if($user==null){
-			$data["message"] = "valeur";
+			$data["message"] = "user inconnu";
 			$this->load->view('Login',$data);
+		}else{
+			$user['password']="";//esorina ny mots de passe-ny
+			$this->session->set_userdata('user',$user);
+			$this->load->view('Accueil');
 		}
-
+	}
+	public function inscription(){
+		$this->load->model('Login');
+		//--donnee
+		$nom=$this->input->post('nom');
+		$mail=$this->input->post('mail');
+		$nee=$this->input->post('nee');
+		$idgenre=$this->input->post('idgenre');
+		$taillecm=$this->input->post('taillecm');
+		$poids=$this->input->post('poids');
+		$password=$this->input->post('password');
+		try{
+			$user=$this->Login->inscriptionUser($nom ,$mail ,$nee ,$idgenre ,$taillecm ,$poids ,$password);
+			$this->load->view('Login');
+		}catch(Exception $ex){
+			$data["message"]=$ex->getMessage();
+			$this->load->view('Inscription',$data);
+		}
 	}
 }
